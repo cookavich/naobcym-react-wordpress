99b24303d6f92fc2298edab3570ba069bc03b7a5
cookavich <paulrcook@me.com>
2017-07-06 20:58:44 -0400

    Fixed some prop errors

--- src/components/PostExcerpt.js
+++ src/components/PostExcerpt.js
@@ -4,8 +4,8 @@ import React from 'react';
 const PostExcerpt = (props) => {
     return (
         <div className="post">
-            <h2><Link to={{pathname: `/post/${props.post.id}`}}>{props.post.title.rendered}</Link></h2>
-            {props.featured_media ?
+            <h2><Link to={{pathname: `/post/${props.post.id}`}}><span dangerouslySetInnerHTML={{__html: props.post.title.rendered}}/></Link></h2>
+            {props.post.featured_media ?
                 <img src={props.post._embedded['wp:featuredmedia'][0].media_details.sizes["full"].source_url}/>
                 : null}
             <p dangerouslySetInnerHTML={{__html: props.post.excerpt.rendered}}/>
